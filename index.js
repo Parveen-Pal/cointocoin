a79b64ce3575e62981d158cbdb1752d4a6951dab
Parveen-Pal <parveenpal02001@gmail.com>
2024-11-03 11:38:48 +0530

    code

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 
 const freecurrencyapi = new Freecurrencyapi('fca_live_bOzSZxjVhGKlpAXXuEUHHbPzLfKYkwDdtuCktf7V');
 
+
+///
 export async function coinExchange(fromCurrency, toCurrency, units) {
     const res = await freecurrencyapi.latest({
         base_currency: fromCurrency,
